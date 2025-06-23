f55dd181b7ad0761b2fda87a64d560611c65840c
Shaun Alexander <shaun.alexander@aa.com>
2025-06-23 15:36:38 -0500

    feat: bump docker image versions and seed service2app table (#57)

--- db/init.sql
+++ db/init.sql
@@ -28,3 +28,23 @@ INSERT INTO mcp_keys (user_id, service, api_key, expiration_date, updated_at, is
 (5, 'github', 'ghp_ExpiredTokenExample123456789012345678', '2024-12-01 00:00:00+00', '2024-10-20 17:25:33+00', false),
 (1, 'jira', '1A2B3C4D5E6F7G8H9I0J1K2L3M4N5O6P7Q8R', '2025-07-10 16:45:00+00', '2025-06-01 12:18:07+00', true),
 (2, 'confluence', 'confluence_api_key_example_abcd1234efgh5678ijkl', NULL, '2025-05-28 15:33:41+00', true);
+
+-- Create sample service2app table
+CREATE TABLE IF NOT EXISTS service2app
+(
+    id SERIAL PRIMARY KEY,
+    jsondata jsonb,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Insert sample data into service2app table
+INSERT INTO service2app VALUES (1, '{"Email": "ims-msg-impl@collins.com", "AppName": "AIRINCCOLLINS", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["AMX1QM1.SP.XMITQ"], "RecordType": "Consumer", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "N/A"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (2, '{"Email": "DL_TechOps_IT_SCEPTRE_Incident_OnCall", "AppName": "SCEPTRE", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["AMX1QM1.SP.XMITQ"], "RecordType": "Publisher", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "SCEPTRE"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (3, '{"Email": "dl_cargo_it_cms_product@aa.com", "AppName": "Inotoc", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["CARGOIBS.DANGERGDS.NTC.01"], "RecordType": "Consumer", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "DANGERGDS"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (4, '{"Email": "N/A", "AppName": "CARGOIBS", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["CARGOIBS.DANGERGDS.NTC.01"], "RecordType": "Publisher", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "CARGOIBS"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (5, '{"Email": "dl_techops_champions@aa.com", "AppName": "JMOCA", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["SCEPTRE.JMOCA.CHECKS.01", "SCEPTRE.JMOCA.MELS.01", "SCEPTRE.JMOCA.MACH.01", "SCEPTRE.JMOCA.MISC.01"], "RecordType": "Consumer", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "JMOCA"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (6, '{"Email": "N/A", "AppName": "SCEPTER", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["SCEPTRE.JMOCA.CHECKS.01", "SCEPTRE.JMOCA.MELS.01", "SCEPTRE.JMOCA.MACH.01", "SCEPTRE.JMOCA.MISC.01"], "RecordType": "Publisher", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "SCEPTER"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (7, '{"Email": "dl_cargo_data_team@aa.com", "AppName": "CRO", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["CARGOIBS.CARGOREVOP.BKG.01"], "RecordType": "Consumer", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "CARGOREVOP"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (8, '{"Email": "N/A", "AppName": "CARGOIBS", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["CARGOIBS.CARGOREVOP.BKG.01"], "RecordType": "Publisher", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "CARGOIBS"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (9, '{"Email": "dl_cargo_data_team@aa.com", "AppName": "CGOREALDTA", "XMatters": "Cargo_DT_DataHub", "DataCenter": "CDC", "QueueNames": ["CARGOIBS.CGOREALDTA.BKG.01"], "RecordType": "Consumer", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "CGOREALDTA"}', '2025-06-13 09:53:57.80939');
+INSERT INTO service2app VALUES (10, '{"Email": "N/A", "AppName": "IBS", "XMatters": "N/A", "DataCenter": "CDC", "QueueNames": ["CARGOIBS.CGOREALDTA.BKG.01"], "RecordType": "Publisher", "Environment": "emq-stage", "QueueManager": "EMQSCXG1", "ArcherShortName": "CARGOIBS"}', '2025-06-13 09:53:57.80939');
